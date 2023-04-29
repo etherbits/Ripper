e706a16d75e6530cb3d1a38fc5ba41f5a4328c22
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-04-29 16:49:59 +0400

    adding setting icon hover

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,13 @@ const App = () => {
     <div className="flex flex-col bg-neutral-950 min-h-screen">
       <header className="flex items-center justify-between p-6">
         <div className="flex items-center gap-1">
-
           <div
             className={`w-6 h-6 bg-indigo-400 [mask-image:url("/assets/icons/arrow-down.svg")] [mask-size:24px]`}
           />
           <h1 className="text-neutral-100 text-xl">Ripper</h1>
         </div>
         <div
-          className={`w-6 h-6 bg-neutral-600 [mask-image:url("/assets/icons/settings.svg")] [mask-size:24px]`}
+          className={`w-6 h-6 bg-neutral-600 [mask-image:url("/assets/icons/settings.svg")] [mask-size:24px] hover:bg-violet-400 cursor-pointer`}
         />
       </header>
       <div className="flex flex-col justify-center items-center"></div>
