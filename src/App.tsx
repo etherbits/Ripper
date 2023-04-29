84a62d277ed1eb2e4658c70d687bf63e684d1b20
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-04-29 16:47:58 +0400

    adding header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@
 //
 // import { invoke } from "@tauri-apps/api/tauri";
 
-function App() {
+const App = () => {
   return (
-    <div className="flex justify-center items-center bg-black min-h-screen">
-      <h1 className="p-4 text-red-500 font-bold text-4xl">RIPPER</h1>
+    <div className="flex flex-col bg-neutral-950 min-h-screen">
+      <header className="flex items-center justify-between p-6">
+        <div className="flex items-center gap-1">
+
+          <div
+            className={`w-6 h-6 bg-indigo-400 [mask-image:url("/assets/icons/arrow-down.svg")] [mask-size:24px]`}
+          />
+          <h1 className="text-neutral-100 text-xl">Ripper</h1>
+        </div>
+        <div
+          className={`w-6 h-6 bg-neutral-600 [mask-image:url("/assets/icons/settings.svg")] [mask-size:24px]`}
+        />
+      </header>
+      <div className="flex flex-col justify-center items-center"></div>
     </div>
   );
-}
+};
 
 export default App;
