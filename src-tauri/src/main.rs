91b9ae270695f764bf92bc3003c162ac53112b1b
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-04-29 17:38:00 +0400

    adding basic tauri command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rustube::{Id, VideoFetcher};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn get_video(url: &str) -> Result<String, ()> {
+   let id = Id::from_raw(url).unwrap();
+    let descrambler = VideoFetcher::from_id(id.into_owned()).unwrap()
+       .fetch()
+       .await.unwrap();
+
+    let video_info = descrambler.video_info();
+    let the_only_truth = &video_info.player_response.video_details.view_count;
+    println!("{}", the_only_truth);
+
+    Ok(format!("{}", the_only_truth))
+
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_video])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-// import { useState } from "react";
-//
-// import { invoke } from "@tauri-apps/api/tauri";
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
 
 const App = () => {
+  const [viewCount, setViewCount] = useState<null | number>(null);
   return (
     <div className="flex flex-col bg-neutral-950 min-h-screen">
       <header className="flex items-center justify-between p-6">
@@ -16,7 +16,23 @@ const App = () => {
           className={`w-6 h-6 bg-neutral-600 [mask-image:url("/assets/icons/settings.svg")] [mask-size:24px] hover:bg-violet-400 cursor-pointer`}
         />
       </header>
-      <div className="flex flex-col justify-center items-center"></div>
+      <div className="flex flex-col justify-center items-center">
+        <span className="text-neutral-100 text-xl"> {viewCount} </span>
+        <button
+          className="bg-violet-500 p-2 rounded-md"
+          onClick={async () => {
+            const views: number = await invoke("get_video", {
+              url: "https://www.youtube.com/watch?v=Z5NoQg8LdDk",
+            })
+
+            setViewCount(views)
+
+          }
+          }
+        >
+          GET DATA
+        </button>
+      </div>
     </div>
   );
 };
